b066122977bee0ae2cb646409830f11933ebddf3
mmicome <mmicome@gmail.com>
2021-10-11 00:54:43 +0800

    feat(): lock.json

--- src/api.js
+++ src/api.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
 
+var instance = axios.create({
+    baseURL: 'http://192.168.230.102:8086',
+    timeout: 1000,
+    withCredentials: true
+});
+
 export function fetchData(key) {
-    return axios.get('/fetchData?key=' + key)
+    return instance.get('/fetchData?key=' + key)
 }
 
 export function changeData() {
-    return axios.post('/changeData')
+    return instance.post('/changeData')
 }
